354f0fca550a78640d27b77e6492970df3547d53
fernargdev <115408504+fernargdev@users.noreply.github.com>
2023-12-12 19:41:54 -0500

    usar array y instalar standard

--- retos/reto-1.js
+++ retos/reto-1.js
@@ -1,21 +1,23 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable no-unused-vars */
+
 // 1 - iterar cada numero
 // 2 - chequeo si el numero esta en el mapa
 // 3 - retorno el que de true
-
 function findFirstRepeated(gifts) {
-  const set = new Set()
+  const list = []
 
   for (const gift of gifts) {
-    if (set.has(gift)) {
+    if (list.includes(gift)) {
       return gift
     }
 
-    set.add(gift)
+    list.push(gift)
   }
 
   return -1
 }
 
-const gifts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 4]
+const gifts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 6]
 
 // console.log(findFirstRepeated(gifts))
